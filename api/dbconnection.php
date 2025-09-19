289a4e069d35886cabe3f17aa8fe4b0d97cd462f
Kassy18 <kassandraandal601@gmail.com>
2025-09-19 22:23:26 +0800

    Update dbconnection.php

--- api/dbconnection.php
+++ api/dbconnection.php
@@ -1,1 +1,23 @@
+<?php
 
+
+$host = 'localhost';
+$database = 'andal_enrollment';
+$user = 'root';
+$password = '';
+$charset = 'utf8mb4';
+
+$options = [
+    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+];
+
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$database;charset=$charset", $user, $password, $options);
+    //echo 'Connected';
+} catch (PDOException $e) {
+    http_response_code(500);
+    echo json_encode(['success' => false, 'message' => 'Database connection error']);
+    exit;
+}
+?>
